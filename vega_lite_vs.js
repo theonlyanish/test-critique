4b6238790be138413023664d4bf6b1911e2bc0f9
Anish Kapse <kapseanish@gmail.com>
2022-10-13 13:16:48 +1100

    Add files via upload

---
+++ vega_lite_vs.js
@@ -0,0 +1,23 @@
+var vg_1 = "new_drop_out.vg.json";
+vegaEmbed("#vg1", vg_1).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "int_visualization.vg.json";
+vegaEmbed("#vg2", vg_2).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "visualization_over_years.vg.json";
+vegaEmbed("#vg3", vg_3).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "visualization_india.vg.json";
+vegaEmbed("#vg4", vg_4).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
